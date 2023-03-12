da027afb0bd626d61210e4a7b6237edce884e38a
Antilochus Spring <antiloch@at-m7.msk.21-school.ru>
2023-03-12 12:47:57 +0300

    Pre-Final

--- src/tests/test_sprintf.c
+++ src/tests/test_sprintf.c
@@ -986,6 +986,17 @@ START_TEST(sprintf_test_sprintf28) {
 }
 END_TEST
 
+START_TEST(sprintf_test_incorrect_specificator) {
+  char str1[BUFF_SIZE];
+  char str2[BUFF_SIZE];
+  char format[] = "%y";
+
+  ck_assert_int_eq(s21_sprintf(str1, format), sprintf(str2, format));
+
+  ck_assert_str_eq(str1, str2);
+}
+END_TEST
+
 Suite *test_sprintf(void) {
   Suite *s = suite_create("\033[1;33m  SPRINTF  \033[0m");
   TCase *tc = tcase_create("sprintf_tc");
@@ -1078,6 +1089,7 @@ Suite *test_sprintf(void) {
   tcase_add_test(tc, sprintf_test_sprintf28);
   tcase_add_test(tc, sprintf_test_sprintf29);
   tcase_add_test(tc, sprintf_test_sprintf30);
+  tcase_add_test(tc, sprintf_test_incorrect_specificator);
 
   suite_add_tcase(s, tc);
   return s;
